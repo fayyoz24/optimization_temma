b707b2982b5b4fcd70c9dc865baf486bc2bf8878
fayyoz24 <fayyozusmon@gmail.com>
2025-03-26 10:39:59 +0500

    fdsf

--- optimization/core/settings.py
+++ optimization/core/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',  # Authentication middleware
-    'temma.middleware.BlockBlockedIPsMiddleware',  # Add custom IP blocking middleware here
+    # 'temma.middleware.BlockBlockedIPsMiddleware',  # Add custom IP blocking middleware here
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
